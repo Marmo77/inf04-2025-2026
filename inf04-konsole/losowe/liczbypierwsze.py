62203b12126fbc13187868d135c2dbfa03c85a48
Marmo77 <matyskamela@gmail.com>
2025-10-29 00:00:22 +0100

    liczby pierwsze - in progress...

---
+++ inf04-konsole/losowe/liczbypierwsze.py
@@ -0,0 +1,12 @@
+class LiczbaPierwsza:
+    # def __init__(self) -> None:
+
+
+    def sito(self):
+        przedzial_liczb = [int(x) for x in range(2,101)]
+
+        print(przedzial_liczb)
+
+
+sol = LiczbaPierwsza()
+sol.sito()
