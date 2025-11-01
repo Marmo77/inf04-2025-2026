c54a32764cc0cc04bcbf07d845afa3afeb084535
Marmo77 <matyskamela@gmail.com>
2025-11-01 23:44:03 +0100

    desktopowy - poprawnie zrobiony folder

--- inf04-2025-czerwiec/desktopowa/inf04-2025-czerwiec/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "inf04_2025_czerwiec")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "inf04_2025_czerwiec.Pages")]
