6c85ba639125a079698089e1dc20366415c94899
Marmo77 <matyskamela@gmail.com>
2025-11-15 19:44:29 +0100

    ai_3: test dzialania

--- inf04-konsole/wygenerowane_AI/ai_3/main.py
+++ inf04-konsole/wygenerowane_AI/ai_3/main.py
@@ -5,3 +5,34 @@ class KontoBankowe:
         self.nazwisko = nazwisko
         self.saldo = saldo
     
+    def wyswietl(self):
+        print(f"Numer konta : {self.numer_konta}")
+        print(f"Właściciel: {self.imie} {self.nazwisko}")
+        print(f"Saldo: {self.saldo}")
+
+    def wplata(self, kwota):
+        if kwota > 0:
+            self.saldo += kwota
+            return True
+        return None
+    def wyplata(self, kwota):
+        if kwota > 0 and self.saldo >= kwota:
+            self.saldo -= kwota
+            return True
+        return False
+
+    def pobierzSaldo(self):
+        return self.saldo
+
+
+Marek = KontoBankowe("2130120321045912", "Marek", "Ziemniak", 250.32)
+
+Marek.wyswietl()
+
+Marek.wplata(250)
+print(f"Saldo po wplacie: {Marek.pobierzSaldo()}")
+
+Marek.wyplata(120)
+print(f"Saldo po wyplacie: {Marek.pobierzSaldo()}")
+
+
