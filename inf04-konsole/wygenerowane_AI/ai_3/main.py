9b5c80b6bfe3bc9490326791faa13adc2310f337
Marmo77 <matyskamela@gmail.com>
2025-11-15 19:41:35 +0100

    ai_3: klasa i metody

---
+++ inf04-konsole/wygenerowane_AI/ai_3/main.py
@@ -0,0 +1,7 @@
+class KontoBankowe:
+    def __init__(self, numer_konta: str, imie: str, nazwisko: str, saldo: float):
+        self.numer_konta = numer_konta
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.saldo = saldo
+    
