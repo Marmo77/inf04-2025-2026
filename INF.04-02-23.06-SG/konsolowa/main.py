8d2966bf2722a869f50b2bb0ae8c02719a073c34
Marmo77 <matyskamela@gmail.com>
2025-12-01 19:01:20 +0100

    konsolowa

---
+++ INF.04-02-23.06-SG/konsolowa/main.py
@@ -0,0 +1,25 @@
+import random
+
+class KlasaSortowan:
+
+    def sortowanie_babelkowe(self, tablica: list[int]):
+        for i in range(len(tablica)):
+            for j in range((len(tablica)-i-1)):
+                if tablica[j] > tablica[j+1]:
+                    tablica[j], tablica[j+1] = tablica[j+1], tablica[j]
+                    
+        
+        print(", ".join(str(x) for x in tablica))
+
+            
+
+
+        # print(tablica)
+
+
+tablica_do_sortowania = [random.randint(1,100) for _ in range(100)]
+
+sortowanie = KlasaSortowan()
+sortowanie.sortowanie_babelkowe(tablica=tablica_do_sortowania)
+
+
