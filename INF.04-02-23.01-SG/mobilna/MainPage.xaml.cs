9ddc0db7fb7d6550b72e1bfc604ec5c493a9af9b
Marmo77 <matyskamela@gmail.com>
2025-12-13 22:55:14 +0100

    mobilna app

---
+++ INF.04-02-23.01-SG/mobilna/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Notes { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Notes = new ObservableCollection<string>
+            {
+                "Zakupy: chleb, masło, ser",
+                "Do zrobienia: obiad, umyć podłogi",
+                "weekend: kino, parking",
+                "spacer z psem",
+            };
+
+            BindingContext = this;
+        }
+
+        private void DopiszDoListy(object sender, EventArgs e)
+        {
+
+            string notatka = TrescNotatki.Text;
+
+            if (!string.IsNullOrEmpty(notatka) )
+            {
+                Notes.Add(notatka);
+                TrescNotatki.Text = "";
+            }
+
+            //    Notes.Add(notatka);
+
+            //    TrescNotatki.Text = "";
+
+            //    string notatki = "";
+
+            //    for (int i=0; i < Notes.Count; i++)
+            //    {
+            //        notatki += $" | {Notes[i]}";
+            //    }
+
+            //    DisplayAlert("HEJ", notatki, "Cancel");
+
+        }
+    }
+}
