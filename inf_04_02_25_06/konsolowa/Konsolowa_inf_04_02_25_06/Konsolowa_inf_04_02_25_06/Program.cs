9ad35638b81eb2f75694fe727c82dbb18a6f321e
Marmo77 <matyskamela@gmail.com>
2026-01-19 14:02:00 +0100

    inf_04_02_25_06 - konsolowka

---
+++ inf_04_02_25_06/konsolowa/Konsolowa_inf_04_02_25_06/Konsolowa_inf_04_02_25_06/Program.cs
@@ -0,0 +1,56 @@
+﻿
+public class Szyfr
+{
+    public string SzyfrCezara(string tekst, int klucz)
+    {
+        tekst = tekst.ToLower();
+        string zaszyfrowany = string.Empty;
+
+        string alfabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // dla kazdej literki w (str)tekst znajdujemy index w (str)alfabet, przesuwamy o (int)klucz, i dodajemy += do zaszyfrowany.
+        for (int i = 0; i < tekst.Length; i++) 
+        {
+            // znalezienie indexu litery w alfabecie
+            int literaIndex = alfabet.IndexOf(tekst[i]);
+
+            // przesuniecie o (int)klucz
+            int nowyIndex = (literaIndex + klucz) % alfabet.Length; // modulo ogranicza aby nie wyjsc poza dlugosci alfabetu
+            if (nowyIndex < 0) // jesli (int)klucz jest na minusie - to sie cofamy od poczatku alfabetu
+            {
+                nowyIndex = nowyIndex + alfabet.Length;
+            }
+
+            if (tekst[i] == ' ') // jesli jest spacja to nie przesuwamy
+            {
+                zaszyfrowany += " ";
+            }
+            else
+            {
+                zaszyfrowany += alfabet[nowyIndex];
+            }
+            //Console.WriteLine($"litera: {tekst[i]} , idx: {literaIndex}, nowy idx: {nowyIndex}");
+        }
+
+        return zaszyfrowany;
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Szyfr cezar = new Szyfr();
+
+        Console.WriteLine("Podaj tekst do zaszyfrowania: ");
+        string tekst = Console.ReadLine();
+        Console.WriteLine("Podaj klucz do przesunięcia: ");
+        int klucz = int.Parse(Console.ReadLine());
+
+        string zaszyfr = cezar.SzyfrCezara(tekst, klucz);
+        Console.WriteLine(zaszyfr);
+    }
+}
---
+++ inf_04_02_25_06/konsolowa/Konsolowa_inf_04_02_25_06/TestyJednostkowe/Test1.cs
@@ -0,0 +1,67 @@
+﻿namespace TestyJednostkowe
+{
+    [TestClass]
+    public sealed class TestySzyfru
+    {
+        [TestMethod]
+        public void DanePodstawoweKluczDodatniKluczMniejszyodDlugosci()
+        {
+            Szyfr szyfr = new Szyfr();
+
+            string tekstWejscie = "abc";
+            int kluczWejscie = 3;
+
+            string oczekiwaneWyjscie = "def";
+
+            Assert.AreEqual(oczekiwaneWyjscie,szyfr.SzyfrCezara(tekstWejscie,kluczWejscie));
+        }
+        [TestMethod]
+        public void ZawijanieLiteryPozaDlugoscAlfabetu()
+        {
+            Szyfr szyfr = new Szyfr();
+
+            string tekstWejscie = "xyz";
+            int kluczWejscie = 3;
+
+            string oczekiwaneWyjscie = "abc";
+
+            Assert.AreEqual(oczekiwaneWyjscie, szyfr.SzyfrCezara(tekstWejscie, kluczWejscie));
+        }
+        [TestMethod]
+        public void OdszyfrowanieKluczujemny()
+        {
+            Szyfr szyfr = new Szyfr();
+
+            string tekstWejscie = "def";
+            int kluczWejscie = -3;
+
+            string oczekiwaneWyjscie = "abc";
+
+            Assert.AreEqual(oczekiwaneWyjscie, szyfr.SzyfrCezara(tekstWejscie, kluczWejscie));
+        }
+        [TestMethod]
+        public void KluczWiekszyNizDlugoscAlfabetu()
+        {
+            Szyfr szyfr = new Szyfr();
+
+            string tekstWejscie = "abc";
+            int kluczWejscie = 29;
+
+            string oczekiwaneWyjscie = "def";
+
+            Assert.AreEqual(oczekiwaneWyjscie, szyfr.SzyfrCezara(tekstWejscie, kluczWejscie));
+        }
+        [TestMethod]
+        public void SpacjaWTekscie()
+        {
+            Szyfr szyfr = new Szyfr();
+
+            string tekstWejscie = "ab cd";
+            int kluczWejscie = 2;
+
+            string oczekiwaneWyjscie = "cd ef";
+
+            Assert.AreEqual(oczekiwaneWyjscie, szyfr.SzyfrCezara(tekstWejscie, kluczWejscie));
+        }
+    }
+}
